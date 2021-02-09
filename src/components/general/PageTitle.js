1ffe6c8bcd2c52fe9422504584b6df185e310da2
MAtterholt <matterholt84@gmail.com>
2021-02-09 00:01:13 -0500

    update feature

--- src/components/general/PageTitle.js
+++ src/components/general/PageTitle.js
@@ -1,8 +1,11 @@
 import { webpageRoutes } from "../../routes/index";
 
 export default function PageTitle({ routerPathName }) {
+
   const pageRouteData = webpageRoutes.find(
     (routeData) => routeData.path === routerPathName
   );
-  return <h2>{pageRouteData.name}</h2>;
+
+
+  return <h2>{pageRouteData ? pageRouteData.name : null}</h2>;
 }
