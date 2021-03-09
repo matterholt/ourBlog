a97863c7e9b35f1c73b1b00b866e1eecba50c8fb
MAtterholt <matterholt84@gmail.com>
2021-03-09 12:34:53 -0500

    update subtitle

--- src/components/Nav/home/SiteTitle.jsx
+++ src/components/Nav/home/SiteTitle.jsx
@@ -4,7 +4,7 @@ import WebSiteSVGIcon from "../../svg/WebSiteSVGIcon";
 
 export default function SiteTitle() {
   return (
-    <div className="grid place-items-center w-3/4">
+    <div className="grid place-items-center w-3/4 md:w-full">
       <HomeLinkContainer>
         <WebSiteSVGIcon styles="m-auto w-48" />
       </HomeLinkContainer>
